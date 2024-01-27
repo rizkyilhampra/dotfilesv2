d2a4eb6d19d79cea224bee3e1570eefc02e04769
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-01-28 05:54:00 +0800

    chore: extend status bar tmux

---
+++ .scripts/tmux-catppuccin-custom-modules/time.sh
@@ -0,0 +1,10 @@
+show_time() {
+  local index=$1
+  local icon="$(get_tmux_option "@catppuccin_time_icon" "")"
+  local color="$(get_tmux_option "@catppuccin_time_color" "$thm_blue")"
+  local text="$(get_tmux_option "@catppuccin_time_text" "$(date +"%H:%M")")"
+
+  local module=$( build_status_module "$index" "$icon" "$color" "$text" )
+
+  echo "$module"
+}
